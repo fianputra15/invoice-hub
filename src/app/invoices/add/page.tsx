0274e88e67675781699723cd7ce32cea07227444
Andi Alfian Pratama Putra <fianputra15798@gmail.com>
2025-02-15 16:56:17 +0700

    feat: initial app

---
+++ src/app/invoices/add/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InvoicesPage = () => {
+  return (
+    <div>
+      <h1>Invoices</h1>
+      <p>This is the Invoices page where you can add new invoices.</p>
+    </div>
+  );
+};
+
+export default InvoicesPage;
---
+++ src/app/invoices/list/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InvoicesListPage = () => {
+  return (
+    <div>
+      <h1>Invoices List</h1>
+      <p>This is the Invoices List page where you can view all invoices.</p>
+    </div>
+  );
+};
+
+export default InvoicesListPage;
